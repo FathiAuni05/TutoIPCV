b61ca0e92fc06b5e3e0fab8c96532649537a271f
Fathi Auni <94261994+FathiAuni05@users.noreply.github.com>
2024-11-20 17:08:40 +0800

    Update test.py

--- test.py
+++ test.py
@@ -7,3 +7,7 @@ rm -rf /home/adminuser/venv
 python3.12 -m venv /home/adminuser/venv
 source /home/adminuser/venv/bin/activate
 pip install -r requirements.txt
+
+python
+>>> import cv2
+>>> print(cv2.__version__)
